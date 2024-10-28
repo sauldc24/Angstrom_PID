1ac908e1cd536f344d1903edbc60a812ce4a8a8d
sauldc24 <sauldc24@gmail.com>
2024-10-28 14:32:42 -0600

    Include ADS1220
    Delete everything related to Arduino Internal ADC and include ADS1220 measurements

--- Angstrom_PID.ino
+++ Angstrom_PID.ino
@@ -3,41 +3,34 @@
 #include <PID_v1.h>
 #include <math.h> // Incluir math.h para usar la función sin()
 
-#define TERMISTOR_PIN A0
-#define TERMISTOR_PIN2 A1
 #define HEAT_OUTPUT 10
 #define COOL_OUTPUT 9
 #define MIN_OUTPUT -255
 #define MAX_OUTPUT 255
 #define ADS1220_CS_PIN    7 // chip select pin para el ADS1220
 #define ADS1220_DRDY_PIN  6 // data ready pin para el ADS1220
-
-const float R_DIV = 10000.0;  // Resistencia del divisor de voltaje
-const float V_REF = 5.0;      // Voltaje de referencia
+#define IDAC_CURRENT 50.0 //corriente del IDAC en uA
 
 // Coeficientes de Steinhart-Hart
 const float A = 1.016156e-03;
 const float B = 2.545381e-04;
 const float C = -9.116603e-09;
 
+/*variables para la medición de voltajes, resistencias y temperaturass. Las variables con el número 1 corresponden a las del termistor
+usado como referencia para el PID, las variables con el número 2 corresponden a las del termistor de monitoreo*/
+float voltage1 = 0.0;
+float voltage2 = 0.0;
+float resistance1 = 0.0;
+float resistance2 = 0.0;
+double temperature1 = 0.0;
+float temperature2 = 0.0;
+
 // Variables para el PID
 double Setpoint, Input, Output;
 double Kp=150., Ki=10., Kd=25.;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID myPID(&temperature1, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 // String para recibir nuevo valor del setpoint proporcionado por el usuario
 String receivedSetpoint; 
-
-const int N = 200; // Número de muestras para promediar
-const int NM = 5000; // Número de milisegundos a los cuales se muestra la temperatura
-
-// Variables para el promediado de voltajes
-float sumaVoltajesInput = 0.0;
-float sumaVoltajesMonitoreo = 0.0;
-int count = 0;
-
-unsigned long ultimoTiempoDisplay = 0;
-
-float tempMonitoreo = 0.0;
 
 // Constantes para la ecuación del setpoint
 const float TEMP_0 = 25.0;    // Temperatura base en °C
@@ -46,8 +39,11 @@ const float FREQ = 0.001;       // Frecuencia de modulación en Hz
 
 //Objeto para controlar el ADS1220
 ADS1220_WE ads = ADS1220_WE(ADS1220_CS_PIN, ADS1220_DRDY_PIN);
-//variable global para guardar el voltaje del termistor
-float voltajeInput;
+
+//variables para determinar el momento en el que se manda la información al puerto serial
+unsigned long ultimoTiempoDisplay = 0;//guarda el último tiempo en el que se mandó la información al puerto serial
+float t = 0;//variable para guardar el tiempo en cada ejecución del bucle loop
+const int NM = 5000; // Número de milisegundos a los cuales se muestra la temperatura
 
 void setup() {
   // Inicializamos el puerto serial
@@ -57,19 +53,12 @@ void setup() {
     while(1);
   }
   //En esta sección se configura el ADS1220 para la lectura del termistor
-  ads.setCompareChannels(ADS1220_MUX_0_3);
   ads.setGain(ADS1220_GAIN_1);
-  ads.bypassPGA(true); //Se desactiva el PGA
-  ads.setIdacCurrent(ADS1220_IDAC_10_MU_A); //Se configura el IDAC1 para una corriente de 10uA (cambiar según sea necesario)
-  ads.setIdac1Routing(ADS1220_IDAC_AIN0_REFP1);//Seleccionar el pin al cual se va a conectar el IDAC
-  // Configuramos los puertos
-  // pinMode(TERMISTOR_PIN, INPUT); // No es necesario para analogRead()
-  pinMode(HEAT_OUTPUT, OUTPUT);
-  pinMode(COOL_OUTPUT, OUTPUT);
-  // Inicializar variables para la entrada del sensor
-  //int lecturaInput = analogRead(TERMISTOR_PIN);
-  voltajeInput = ads.getVoltage_mV();//leemos el voltaje del ADS1220 directamente en milivoltios
-  Input = calcularTemperatura(voltajeInput, 10);
+  ads.bypassPGA(true); // true disables PGA, false enables PGA
+  ads.setIdacCurrent(ADS1220_IDAC_50_MU_A);//Este hay que ponerlo manualmente por que las definiciones dependen de los registros
+  ads.setIdac1Routing(ADS1220_IDAC_AIN0_REFP1);//IDAC 1 en el pin AIN0
+  ads.setIdac2Routing(ADS1220_IDAC_AIN1);//IDAC 2 en el pin AIN1
+
   // Setpoint inicial
   Setpoint = TEMP_0;
   // Definimos los límites para la salida del controlador
@@ -82,40 +71,21 @@ void loop() {
   unsigned long tiempoActual = millis();
   // Calcular el tiempo en segundos
   float t = tiempoActual / 1000.0;
-
   // Actualizar el setpoint según la ecuación dada
   Setpoint = TEMP_0 + AMP * sin(2 * PI * FREQ * t);
-
+  /*El bucle if revisa si hay datos en el puerto serial para actualizar el set point*/
   if (Serial.available() > 0){
     Setpoint = Serial.parseFloat();
     Serial.readStringUntil('\n'); // Limpiar el buffer
   }
 
-  // Leer voltajes de los termistores y acumular sumas
-  //int lecturaInput = analogRead(TERMISTOR_PIN);
-  voltajeInput = ads.getVoltage_mV();
-  sumaVoltajesInput += voltajeInput;
-
-  int lecturaMonitoreo = analogRead(TERMISTOR_PIN2);
-  float voltajeMonitoreo = lecturaMonitoreo * (V_REF / 1023.0);
-  sumaVoltajesMonitoreo += voltajeMonitoreo;
-
-  count++;
-
-  if (count >= N) {
-    // Calcular voltajes promediados
-    float promedioVoltajeInput = sumaVoltajesInput / count;
-    float promedioVoltajeMonitoreo = sumaVoltajesMonitoreo / count;
-
-    // Calcular temperaturas a partir de los voltajes promediados
-    Input = calcularTemperatura(promedioVoltajeInput);
-    tempMonitoreo = calcularTemperatura(promedioVoltajeMonitoreo);
-
-    // Reiniciar sumas y contador
-    sumaVoltajesInput = 0.0;
-    sumaVoltajesMonitoreo = 0.0;
-    count = 0;
-  }
+  // En esta sección se leen los voltajes y temperaturas de los sensores
+  ads.setCompareChannels(ADS1220_MUX_0_3);//configurar el multiplexor en los pines del termistor 1
+  voltage1 = ads.getVoltage_mV(); // voltaje del termistor 1
+  temperature1 = calcularTemperatura(voltage1, IDAC_CURRENT);
+  ads.setCompareChannels(ADS1220_MUX_1_2);//configurar el multiplexor en los pines del termistor 2
+  voltage2 = ads.getVoltage_mV(); // voltaje del termistor 2
+  temperature2 = calcularTemperatura(voltage1, IDAC_CURRENT);
 
   // Calcular PID y controlar salida continuamente
   myPID.Compute();
@@ -124,14 +94,13 @@ void loop() {
   // Mostrar temperaturas cada NM milisegundos
   if (tiempoActual - ultimoTiempoDisplay >= NM) {
     ultimoTiempoDisplay = tiempoActual;
-
     Serial.print(TEMP_0 + AMP);
     Serial.print(", ");
     Serial.print(TEMP_0 - AMP);
     Serial.print(", ");
-    Serial.print(Input);
+    Serial.print(temperature1);
     Serial.print(",   ");
-    Serial.println(tempMonitoreo);
+    Serial.println(temperature2);
   }
 }
 
